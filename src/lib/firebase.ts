61411bcf6708252538bc560a7fc02fc770b28e2a
Patrick Doran <patrickdoranlearning@gmail.com>
2025-08-10 15:26:04 +0000

    Set up Firebase Backend

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "hortitrack",
-  appId: "1:841216037830:web:58337596518973b00998c7",
-  storageBucket: "hortitrack.firebasestorage.app",
-  apiKey: "AIzaSyAsqgI_D4viEIh7q146pD2WSLdUxx_2Qa8",
-  authDomain: "hortitrack.firebaseapp.com",
-  messagingSenderId: "841216037830",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
