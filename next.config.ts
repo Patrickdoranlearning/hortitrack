e2bb5168f85bc1ad2ce111b6747ed3e0430a9a09
Patrick Doran <patrickdoranlearning@gmail.com>
2025-08-11 08:35:57 +0000

    Changes before Firebase Studio auto-run

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: [
+      'https://9004-firebase-studio-1754759437684.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'
+    ],
+  },
   devIndicators: {
     allowedDevOrigins: [
         '*.cloudworkstations.dev',
