86622c902af402c6e0efff770ec735a1ed653aeb
Patrick Doran <patrickdoranlearning@gmail.com>
2025-08-11 08:55:25 +0000

    First push

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   experimental: {
-    allowedDevOrigins: [
-      'https://9004-firebase-studio-1754759437684.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev'
-    ],
-  },
-  devIndicators: {
     allowedDevOrigins: [
         '*.cloudworkstations.dev',
     ]
@@ -27,6 +22,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+export default nextConfig
