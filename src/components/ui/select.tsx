756af692c2410cbf98f5c12e51d7a6d81157363e
Patrick Doran <patrickdoranlearning@gmail.com>
2025-08-12 10:18:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -129,7 +129,8 @@ const SelectItem = React.forwardRef<
         <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
-    <span>{children}</span>
+
+    {children}
   </SelectPrimitive.Item>
 ))
 SelectItem.displayName = SelectPrimitive.Item.displayName
