080c42f92bd161bd76b9c124d9fc5a9b479aba9c
Patrick Doran <patrickdoranlearning@gmail.com>
2025-08-14 09:59:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -7,7 +7,7 @@ import { getAuth } from "firebase-admin/auth";
 
 const projectId = process.env.FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"); // important!
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
 // Fail fast with a helpful message if env is missing
 if (!projectId || !clientEmail || !privateKey) {
