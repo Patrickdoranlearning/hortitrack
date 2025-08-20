3de55a80370d8cfaf80ccf7e076437b28e16042b
Patrick Doran <patrickdoranlearning@gmail.com>
2025-08-20 15:00:48 +0000

    Okay the action icon still isn't obvious. Lets make it the outline of a

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,3 @@
-
 /**
  * @fileoverview This file contains reusable icon components for the application.
  */
@@ -16,11 +15,11 @@ export const TransplantIcon = () => (
       strokeLinejoin="round"
       className="h-5 w-5"
     >
-      <path d="M7 22h10" />
-      <path d="M12 17V7" />
-      <path d="M12 7a2.5 2.5 0 0 1 0-5c.9 0 1.8.5 2.4 1.2A3.2 3.2 0 0 1 15.5 5" />
-      <path d="M12 7a2.5 2.5 0 0 0 0-5c-.9 0-1.8.5-2.4 1.2A3.2 3.2 0 0 0 8.5 5" />
-      <path d="m14 14-2 2-2-2" />
+      <path d="M12 22v-6" />
+      <path d="M12 16a6 6 0 0 1 6-6v-2h-2" />
+      <path d="M12 16a6 6 0 0 0-6-6v-2h2" />
+      <path d="M12 10V4" />
+      <path d="m15 7-3-3-3 3" />
     </svg>
   );
 
@@ -37,9 +36,10 @@ export const CareIcon = () => (
         strokeLinejoin="round"
         className="h-5 w-5"
     >
-        <path d="M14 10V8a2 2 0 1 0-4 0v2" />
-        <path d="M18.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" />
-        <path d="M10.4 18.9c-2 .8-6.4 1.1-6.4 1.1s-.8-4.4 1.1-6.4" />
-        <path d="M14.8 17.2c.8-2 .5-5.2.5-5.2s-3.2.3-5.2.5" />
+       <path d="M18.4 12.6a1.5 1.5 0 0 0-2.1-2.1l-2.4 2.4-2.1-2.1a1.5 1.5 0 0 0-2.1 2.1l2.4 2.4-2.4 2.4a1.5 1.5 0 0 0 2.1 2.1l2.1-2.1 2.1 2.1a1.5 1.5 0 0 0 2.1-2.1l-2.4-2.4 2.4-2.4Z" />
+       <path d="M14 19.5V14l-2.5 2.5" />
+       <path d="m10 16.5 2.5-2.5" />
+       <path d="M20 13.5V10c0-2.5-2-4-4-4" />
+       <path d="M4 10a4 4 0 0 1 4-4" />
     </svg>
 );
